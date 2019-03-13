b4b75d0751bb06ecf063af21a357ba7407f526dc
Maxim Therrien <maxim@acerspyro.ca>
2019-03-13 13:15:24 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+//// DECLARATIONS
+
+const Features = {
+	ASSET_NAME,
+  ASSET_SERIAL,
+  ASSET_TAG,
+  ASSET_MODEL,
+  COMPANY_NAME,
+  CODE_2D,
+  CODE_1D
+}
+
+const Code2DTypes = {
+	QRCODE,
+  DATAMATRIX
+}
+
+const Code1DTypes = {
+	C128,
+  C39,
+  PDF417,
+  EAN5
+}
+
